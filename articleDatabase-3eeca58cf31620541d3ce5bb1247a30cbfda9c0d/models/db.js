9e6ce61b5bd6983f63e0b9c19681fdb9c66815fd
Ted Yadav <yadavdikshant0@gmail.com>
2020-06-16 21:24:04 +1200

    commit

--- articleDatabase-3eeca58cf31620541d3ce5bb1247a30cbfda9c0d/models/db.js
+++ articleDatabase-3eeca58cf31620541d3ce5bb1247a30cbfda9c0d/models/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb://localhost:27017/ArticleDB',{useNewUrlParser: true, useUnifiedTopology: true },(err)=>{
+var mongodb = 'mongodb://localhost:27017/ArticleDB';
+//mongoose.connect('mongodb://localhost:27017/ArticleDB',{useNewUrlParser: true, useUnifiedTopology: true },(err)=>{
+mongoose.connect(mongodb, {useNewUrlParser: true, useUnifiedTopology: true },(err)=>{
     if (!err){console.log('MongoDB COnnection Succeded')}
     else{console.log('Error in DB connection: '+ err)}
 });
