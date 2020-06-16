29caac9133f9da9b8053d0887a03c94e3ed848f6
DTVizzle <55521080+DTVizzle@users.noreply.github.com>
2020-06-16 20:28:51 +1200

    Update to server.js

--- articleDatabase-3eeca58cf31620541d3ce5bb1247a30cbfda9c0d/models/db.js
+++ articleDatabase-3eeca58cf31620541d3ce5bb1247a30cbfda9c0d/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb://localhost:27017/ArticleDB',{useNewUrlParser: true, useUnifiedTopology: true },(err)=>{
+mongoose.connect('mongodb+srv://dbUser:KobeBryant24@cluster0-jnvsr.mongodb.net/ArticleDB?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true },(err)=>{
     if (!err){console.log('MongoDB COnnection Succeded')}
     else{console.log('Error in DB connection: '+ err)}
 });
